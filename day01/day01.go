f04f738f3d20ab3e09be7f2a2634d4ea7cdd33fa
Ballihaut <theo.ballihaut@gmail.com>
2024-09-16 16:26:19 +0200

    main: add main starter, day01: add dummy function to connect main starter

---
+++ day01/day01.go
@@ -0,0 +1,7 @@
+package day01
+
+import "fmt"
+
+func Solve(input string) {
+	fmt.Println("H3110 W0RLD5")
+}
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	d01 "./day01/"
+)
+
+var (
+	inputFile string
+	testMode  bool
+	exercise  uint
+)
+
+func init() {
+	flag.StringVar(&inputFile, "f", "", "Path to the input file.")
+	flag.BoolVar(&testMode, "t", false, "Enable test mode for default path.")
+	flag.UintVar(&exercise, "d", 1, "The number of the day you want to solve.")
+	flag.Parse()
+
+	if inputFile == "" { // no input file, switch to default
+		if testMode {
+			inputFile = "./tinput/" // default path for test mode
+		} else {
+			inputFile = "./input/" // default path for inputs
+		}
+		if exercise < 10 { // paddle with 0 as each file is dayXX
+			inputFile += fmt.Sprint("day0%d", exercise)
+		} else {
+			inputFile += fmt.Sprint("day%d", exercise)
+		}
+		fmt.Printf("Using default path: %s", inputFile) // notifying user
+	}
+}
+
+func main() {
+
+	inputBytes, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading `%s`: %w", inputFile, err)
+		return
+	}
+
+	switch exercise {
+	case 1:
+		d01.Solve(string(inputBytes))
+	default:
+		fmt.Printf("%d is not implemented yet")
+	}
+}
