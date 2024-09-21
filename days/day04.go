e880515a35ab6e0b85c093c647c3ce7f81588917
Ballihaut <theo.ballihaut@gmail.com>
2024-09-21 10:14:42 +0200

    day04: part 2

--- days/day04.go
+++ days/day04.go
@@ -7,20 +7,41 @@ import (
 
 // Resolves Aoc 2023 day 04 challenges.
 func Day04(input string) {
-	var sum uint
+	var (
+		sum    uint
+		points uint
+
+		matching        uint
+		scratchcards    []uint
+		scratchcardsSum uint
+	)
+
+	scratchcards = make([]uint, 256) //	large enough to contain the 200+ lines
 
 	lines := strings.Split(input, "\n")
 
-	for _, line := range lines {
-		sum += linePoints(line)
+	for i, line := range lines {
+		scratchcards[i] += 1
+		points, matching = linePoints(line)
+		sum += points
+
+		for j := uint(1); j <= matching; j++ {
+			scratchcards[uint(i)+j] += scratchcards[i]
+		}
+	}
+
+	for i := 0; i < len(lines); i++ {
+		scratchcardsSum += scratchcards[i]
 	}
 
 	fmt.Printf("Day 04 solution 1 is %d\n", sum)
+	fmt.Printf("Day 04 solution 2 is %d\n", scratchcardsSum)
+
 }
 
 // linePoints counts the points gained on a line.
 // It trims by itself the line Id.
-func linePoints(line string) uint {
+func linePoints(line string) (points, matchingNumbers uint) {
 	line = trimId(line) // removes the "Id" part at the beginning.
 
 	winNumbersString, numbersString, found := strings.Cut(line, "|") // separates the winning numbers from the numbers.
@@ -34,11 +55,10 @@ func linePoints(line string) uint {
 	numbersString = strings.TrimSpace(numbersString)
 	numbers := strings.Split(numbersString, " ")
 
-	var points uint = 0
-
 	for _, n := range numbers {
 		for _, wn := range winNumbers {
 			if n == wn && n != "" {
+				matchingNumbers++
 				if points == 0 {
 					points += 1
 				} else {
@@ -48,5 +68,5 @@ func linePoints(line string) uint {
 		}
 	}
 
-	return points
+	return
 }
